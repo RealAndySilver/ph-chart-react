7855496bc9ac314e6cba7fe603ab4ac62e8c8f6f
Parzifal de León <parzifal.clavijo@apriorins.com>
2017-07-26 16:19:30 -0500

    change port to 9090

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = env => {
     },
     devtool: ifProd('source-map', 'eval'),
     devServer: {
-      port: 8080,
+      port: 9090,
       historyApiFallback: true
     },
     module: {
